03b3456061e0957d009d91eebdf0fe6b146d9926
Mehedi Hasan Riad <urmakecodebg@gmail.com>
2024-12-03 01:22:30 +0600

    Add files via upload

---
+++ C Flow Control/C switch Statement/Syntax of switch...case.c
@@ -0,0 +1,15 @@
+switch (expression)
+​{
+    case constant1:
+      // statements
+      break;
+
+    case constant2:
+      // statements
+      break;
+    .
+    .
+    .
+    default:
+      // default statements
+}
